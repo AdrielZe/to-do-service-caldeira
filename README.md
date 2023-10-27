1d2d7bddb663157c22960c1ccd85ba97a46d37cf
AdrielZe <oadriel14@gmail.com>
2023-10-27 20:49:53 -0300

    Adicionando EndPoints /tasks e /tasks/add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# to-do-service-caldeira
+<h1> Projeto To-Do Service</h1>
+<p>Esse repositório é dedicado ao projeto To Do Service, proposto pelo programa Geração Caldeira 2023.</p>
--- demo/src/main/java/com/example/demo/TaskController.java
+++ demo/src/main/java/com/example/demo/TaskController.java
@@ -1,17 +1,50 @@
 package com.example.demo;
 
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.*;
+import java.util.ArrayList;
+
 @RestController
 
 public class TaskController {
-    private Task tasksList[];
+    public ArrayList<Task> tasks = new ArrayList<Task>();
 
-    @PostMapping("/tasks")
-    public String createTask(){
-        return "teste";
+    @GetMapping("/tasks")
+    public String listTasksText(){
+        return "Listar tasks";
+    }
+
+    @PostMapping(value = "/tasks")
+    public void listTaks(){
+        System.out.println("Added tasks: ");
+        for (Task x: tasks) {
+            System.out.println("----------");
+            System.out.println("Id: " + x.getId());
+            System.out.println("Description: " + x.getDescription());
+            System.out.println("Data de validade: " + x.getExpirationDate());
+            System.out.println("Done: " + x.isDone());
+            System.out.println("----------");
+        }
+    }
+
+    @GetMapping("/tasks/add")
+    public String index(){
+        return "Adicionar tasks";
+    }
+
+    @PostMapping(value = "/tasks/add", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Task createTask(@RequestBody Task task){
+        System.out.println(task.getId());
+        System.out.println(task.getDescription());
+        System.out.println(task.getExpirationDate());
+        System.out.println(task.isDone());
+        tasks.add(task);
+        return task;
     }
 }
